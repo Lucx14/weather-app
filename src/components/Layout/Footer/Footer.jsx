3aebbd8ea14bbfaa90fdc7ae43aae6b48f1d7f6c
Lucien Najev <luciennajev@hotmail.com>
2020-05-08 13:44:58 +0100

    style header footer and background image

--- src/components/Layout/Footer/Footer.jsx
+++ src/components/Layout/Footer/Footer.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
-  background-color: pink;
+  background-color: black;
+  color: white;
+  opacity: 0.5;
   position: absolute;
   bottom: 0px;
   width: 100%;
@@ -10,15 +12,17 @@ const Wrapper = styled.div`
 `;
 
 const InnerWrapper = styled.div`
-  border: solid black 1px;
+  /* border: solid black 1px; */
   margin: 2.4rem;
+  text-align: center;
 `;
 
 const Footer = () => {
   return (
     <Wrapper>
       <InnerWrapper>
-        <h1>Footer</h1>
+        <p>Designed and developed by Lucx14</p>
+        <p>Code on github</p>
       </InnerWrapper>
     </Wrapper>
   );
--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
-  background-color: grey;
+  background-color: black;
+  opacity: 0.7;
   position: absolute;
   top: 0px;
   width: 100%;
@@ -10,15 +11,30 @@ const Wrapper = styled.div`
 `;
 
 const InnerWrapper = styled.div`
-  border: solid black 1px;
   margin: 2.4rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: white;
+`;
+
+const IconsWrapper = styled.div`
+  width: 300px;
+  display: flex;
+  justify-content: space-evenly;
 `;
 
 const Header = () => {
   return (
     <Wrapper>
       <InnerWrapper>
-        <h1>Weather App</h1>
+        <h1>Weather Forecast</h1>
+        <IconsWrapper>
+          <i className="owf owf-800 owf-3x" />
+          <i className="owf owf-202 owf-3x" />
+          <i className="owf owf-957 owf-3x" />
+          <i className="owf owf-903 owf-3x" />
+        </IconsWrapper>
       </InnerWrapper>
     </Wrapper>
   );
